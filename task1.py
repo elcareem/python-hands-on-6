e5a0badb8875fa0088f647ef2974e149ee667c2f
your_name <andrewmiracle100@gmail.com>
2025-08-29 15:54:10 +0100

    initial commit

---
+++ task1.py
@@ -0,0 +1,36 @@
+"""
+You are given a list of bank transactions:
+
+transactions = [
+    {"id": 1, "amount": 200, "type": "deposit"},
+    {"id": 2, "amount": 50, "type": "withdraw"},
+    {"id": 3, "amount": 500, "type": "deposit"}
+]
+
+function Signature
+def filter_transactions(transactions, criteria):
+    pass
+
+Requirement(s):
+The function should return a list of transactions that match the given criteria.
+
+1. If the criteria is a string field (e.g., "type": "deposit"), return only those transactions with the exact same type.
+    filter_transactions(transactions, {"type": "deposit"})
+ → Returns all deposit transactions
+
+2.   If the criteria is a numeric field (e.g., "amount": 100), return all transactions where the value is greater than or equal to the given number.
+
+filter_transactions(transactions, {"amount": 100})
+ → Returns all transactions with amount ≥ 100
+
+
+ Expected Output Examples:
+print(filter_transactions(transactions, {"type": "deposit"}))
+[{'id': 1, 'amount': 200, 'type': 'deposit'},
+{'id': 3, 'amount': 500, 'type': 'deposit'}]
+
+print(filter_transactions(transactions, {"amount": 100}))
+[{'id': 1, 'amount': 200, 'type': 'deposit'},
+{'id': 3, 'amount': 500, 'type': 'deposit'}]
+
+"""
---
+++ task4.py
@@ -0,0 +1,16 @@
+"""
+Task: School Attendance Tracker
+
+Write a function check_attendance(students, present_list) that takes:
+students: a list of all student names
+present_list: a list of students who are present today
+Return a dictionary with students as keys and "Present" or "Absent" as values
+
+students = ["John", "Mary", "Peter", "Alice"]
+present_list = ["John", "Alice"]
+print(check_attendance(students, present_list))
+Expected Output:
+
+{'John': 'Present', 'Mary': 'Absent', 'Peter': 'Absent', 'Alice': 'Present'}
+
+"""
