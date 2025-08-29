51e10a4aa71e57c0e404585ba6fba6ec6c80f6b6
your_name <andrewmiracle100@gmail.com>
2025-08-29 17:06:43 +0100

    complete task 3

--- task3.py
+++ task3.py
@@ -18,3 +18,18 @@ print(calculate_grade(60))   # D
 print(calculate_grade(70))   # C
 print(calculate_grade(89))   # B
 """
+
+def calculate_grade(score):
+	if 90 <= score <= 100:
+		return 'A'
+	elif 80 <= score < 90:
+		return 'B'
+	elif 70 <= score < 80:
+		return 'C'
+	elif 60 <= score < 70:
+		return 'D'
+	elif 0 <= score < 60:
+		return 'F'
+	else:
+		"Invalid Score"
+
